fbb9747b2d56b421a76c1b051f9829902ac579e7
venomega <guilleps92@gmail.com>
2021-03-24 22:25:20 -0400

    updated

--- curses/win.py
+++ curses/win.py
@@ -2,10 +2,15 @@ import curses
 
 
 class window():
+    h = 0
+    w = 0 
     def __init__(self, h=0, w=0, y=0, x=0):
         count = 0
         ww = curses.COLS
         hh = curses.LINES
+        self.m = 0
+        self.buffer = []
+        self.changed = False
         #if there is a float on [h, w]
         if type(float()) in [type(h), type(w)]:
             self.x = int(ww * x)
@@ -39,6 +44,31 @@ class window():
         self.win.addstr(y, x, string)
         self.refresh()
 
+    def buffer_add(self, string):
+        """
+        Add string to buffer, for the record
+        """
+        self.buffer.append(string)
+
+    def write_buffer(self):
+        """
+        Write all the buffer to screen
+        """
+        self.clean()
+        count = 1
+        if len(self.buffer) <= (self.h - 2):
+            for lines in range(0,len(self.buffer)):
+                self.write(count, 1, self.buffer[lines])
+                count+=1
+        else:
+            token = len(self.buffer) - self.h - 2
+            count = 1
+            #for lines in range(token - self.m - 2, len(self.buffer) - self.m - 3):
+            for lines in range(self.h - 2):
+                self.write(count, 1, str(self.buffer[(self.m+self.h - 1 - count)* - 1]))
+                #self.write(1, 1, str(range(token - self.m, len(self.buffer) - self.m)))
+                count+=1
+
     def clean(self):
         """
         Clean the whole screen
@@ -55,6 +85,26 @@ class window():
         Change the cursor to the start of the window
         """
         self.write(0, 0, "")
+
+    def up_marging(self):
+        """
+        Add 1 to the bookmark
+        """
+        if (self.m + len(self.buffer)) <= (self.h - 2):
+            pass
+        else:
+            self.m += 1
+            self.changed = True
+
+    def down_marging(self):
+        """
+        Add -1 to the bookmark
+        """
+        if self.m == 0:
+            pass
+        else:
+            self.m -= 1
+            self.changed = True
 
     def scroll_on(self):
         """
